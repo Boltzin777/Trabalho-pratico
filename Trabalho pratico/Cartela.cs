f61be64a17c7773cd40b9903c02ba8353d794257
canal <canal@LUCAS>
2024-12-07 19:49:41 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho pratico/Cartela.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_pratico
+{
+    internal class Cartela
+    {
+        public int[,] cartelas;
+        private int[] numsorts;
+
+        public int Prenchercartela()
+        {
+            
+        }
+        public int Verificarcartelaunica()
+        {
+
+        }
+
+    }
+}
